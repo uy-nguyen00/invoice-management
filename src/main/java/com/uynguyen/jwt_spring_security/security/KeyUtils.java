a19aa855904f967eb61a517563c9fb7b7e7eccee
Uy Nguyen <uynguyen1501@gmail.com>
2025-08-10 12:35:10 +0200

    implement the KeyUtils service
    NOTE: for the scope of this project, we store the keys locally

---
+++ src/main/java/com/uynguyen/jwt_spring_security/security/KeyUtils.java
@@ -0,0 +1,45 @@
+package com.uynguyen.jwt_spring_security.security;
+
+import java.io.InputStream;
+import java.security.KeyFactory;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.util.Base64;
+
+public class KeyUtils {
+
+    public KeyUtils() {
+    }
+
+    public static PrivateKey loadPrivateKey(final String pemPath) throws Exception {
+        final String key = readKeyFromResource(pemPath)
+                .replace("-----BEGIN PRIVATE KEY-----", "")
+                .replace("-----END PRIVATE KEY-----", "")
+                .replaceAll("\\s+", "");
+
+        final byte[] decoded = Base64.getDecoder().decode(key);
+        final PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(decoded);
+        return KeyFactory.getInstance("RSA").generatePrivate(spec);
+    }
+
+    public static PublicKey loadPublicKey(final String pemPath) throws Exception {
+        final String key = readKeyFromResource(pemPath)
+                .replace("-----BEGIN PUBLIC KEY-----", "")
+                .replace("-----END PUBLIC KEY-----", "")
+                .replaceAll("\\s+", "");
+
+        final byte[] decoded = Base64.getDecoder().decode(key);
+        final PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(decoded);
+        return KeyFactory.getInstance("RSA").generatePublic(spec);
+    }
+
+    private static String readKeyFromResource(String pemPath) throws Exception {
+        try (final InputStream is = KeyUtils.class.getResourceAsStream(pemPath)) {
+            if (is == null) {
+                throw new IllegalArgumentException("Could not find key file " + pemPath);
+            }
+            return new String(is.readAllBytes());
+        }
+    }
+}
