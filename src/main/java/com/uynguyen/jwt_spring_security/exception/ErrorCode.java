8c5e13d894b82968ba8108f115d6fbba844c527b
Uy Nguyen <uynguyen1501@gmail.com>
2025-09-27 21:53:36 +0200

    implement the authentication logic

--- src/main/java/com/uynguyen/jwt_spring_security/exception/ErrorCode.java
+++ src/main/java/com/uynguyen/jwt_spring_security/exception/ErrorCode.java
@@ -10,6 +10,8 @@ public enum ErrorCode {
 
     USER_NOT_FOUND("USER_NOT_FOUND", "User not found with id %s", HttpStatus.NOT_FOUND),
 
+    PASSWORDS_MISMATCH("PASSWORD_MISMATCH", "Passwords do not match", HttpStatus.BAD_REQUEST),
+
     CHANGE_PASSWORD_MISMATCH("CHANGE_PASSWORD_MISMATCH", "New password and confirmed password must be the same", HttpStatus.BAD_REQUEST),
 
     INVALID_OLD_PASSWORD("INVALID_OLD_PASSWORD", "Old password is invalid", HttpStatus.BAD_REQUEST),
@@ -18,7 +20,13 @@ public enum ErrorCode {
 
     ACCOUNT_ALREADY_ACTIVATED("ACCOUNT_ALREADY_ACTIVATED", "Account already activated", HttpStatus.BAD_REQUEST),
 
-    INVALID_JWT_TOKEN("INVALID_JWT_TOKEN", "Invalid JWT token", HttpStatus.BAD_REQUEST);
+    INVALID_JWT_TOKEN("INVALID_JWT_TOKEN", "Invalid JWT token", HttpStatus.BAD_REQUEST),
+
+    EMAIL_ALREADY_EXISTS("EMAIL_ALREADY_EXISTS", "Email already exists", HttpStatus.CONFLICT),
+
+    PHONE_ALREADY_EXISTS("PHONE_ALREADY_EXISTS", "Phone number already exists", HttpStatus.CONFLICT),
+
+    ;
 
     private final String code;
     private final String defaultMessage;
--- src/main/java/com/uynguyen/jwt_spring_security/user/UserMapper.java
+++ src/main/java/com/uynguyen/jwt_spring_security/user/UserMapper.java
@@ -1,14 +1,31 @@
 package com.uynguyen.jwt_spring_security.user;
 
+import com.uynguyen.jwt_spring_security.auth.request.RegistrationRequest;
 import com.uynguyen.jwt_spring_security.user.request.ProfileUpdateRequest;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserMapper {
 
-    public void mergeUserInfo(User user, ProfileUpdateRequest request) {
+    public void mergeUserInfo(final User user, final ProfileUpdateRequest request) {
         user.setFirstName(request.getFirstName());
         user.setLastName(request.getLastName());
         user.setDateOfBirth(request.getDateOfBirth());
+    }
+
+    public User toUser(final RegistrationRequest request) {
+        return User
+                .builder()
+                .firstName(request.getFirstName())
+                .lastName(request.getLastName())
+                .email(request.getEmail())
+                .phoneNumber(request.getPhoneNumber())
+                .password(request.getPassword())
+                .enabled(true) // can change after to implement email / phone verification
+                .locked(false)
+                .credentialsExpired(false)
+                .emailVerified(false)
+                .phoneVerified(false)
+                .build();
     }
 }
