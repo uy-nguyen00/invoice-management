27197028364159c656cd121b19dd9358adabe9f9
Uy Nguyen <uynguyen1501@gmail.com>
2025-09-28 02:18:42 +0200

    add validation to auth requests

--- src/main/java/com/uynguyen/jwt_spring_security/auth/request/AuthenticationRequest.java
+++ src/main/java/com/uynguyen/jwt_spring_security/auth/request/AuthenticationRequest.java
@@ -1,5 +1,8 @@
 package com.uynguyen.jwt_spring_security.auth.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 @Getter
@@ -9,7 +12,12 @@ import lombok.*;
 @Builder
 public class AuthenticationRequest {
 
-    // validation later
+    @NotBlank(message = "VALIDATION.AUTHENTICATION.EMAIL.NOT_BLANK")
+    @Email(message = "VALIDATION.AUTHENTICATION.EMAIL.FORMAT")
+    @Schema(example = "example@mail.com")
     private String email;
+
+    @NotBlank(message = "VALIDATION.AUTHENTICATION.PASSWORD.NOT_BLANK")
+    @Schema(example = "<PASSWORD>")
     private String password;
 }
