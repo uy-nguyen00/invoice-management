a36f593e5f7806ba2d985ccc7ae9f75fbae013b6
Uy Nguyen <uynguyen1501@gmail.com>
2025-09-28 18:55:45 +0200

    openapi doc

---
+++ src/main/java/com/uynguyen/jwt_spring_security/config/OpenApiConfig.java
@@ -0,0 +1,47 @@
+package com.uynguyen.jwt_spring_security.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+        info = @Info(
+                contact = @Contact(
+                        email = "uynguyen1501@gmail.com",
+                        url = "https://github.com/uy-nguyen00"
+                ),
+                description = "OpenApi documentation",
+                title = "OpenApi specification",
+                version = "1.0",
+                termsOfService = "Terms of service"),
+        servers = {
+                @Server(
+                        description = "Local ENV",
+                        url = "http://localhost:8080"
+                ),
+                @Server(
+                        description = "PROD ENV",
+                        url = "https://your-prod-url.com"
+                )
+        },
+        security = {
+                @SecurityRequirement(
+                        name = "bearerAuth"
+                )
+        }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        description = "JWT auth description",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+public class OpenApiConfig {
+}
